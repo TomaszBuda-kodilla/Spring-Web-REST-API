c3ba047471a1fd505709d1449ccf2024bde4f2d3
leetdco2@gmail.com <leetdco2@gmail.com>
2020-05-13 19:03:13 +0200

    19.3 - 1st try

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -16,7 +17,16 @@ public class DbService {
         return repository.findAll();
     }
 
-    public List<Task> getOne(long id){
-        return repository.findById(1l);
+    public Optional<Task> getTask(final Long id){
+        return repository.findById(id);
     }
+
+    public Task saveTask(final Task task){
+        return repository.save(task);
+    }
+
+    public void deleteTask(final Long id){
+        repository.deleteById(id);
+    }
+
 }
