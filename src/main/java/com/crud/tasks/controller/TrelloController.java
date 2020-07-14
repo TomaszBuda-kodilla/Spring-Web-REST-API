affd8591e22324effa90ef4f667d1c755083f627
leetdco2@gmail.com <leetdco2@gmail.com>
2020-07-14 09:04:58 +0200

    22.3 - questions

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,9 +1,10 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.*;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +24,29 @@ public class TrelloController {
     public void getTrelloBoards() {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        trelloBoards.forEach(trelloBoardDto -> { System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
 
-        //trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+            System.out.println("This board contains lists: ");
 
-        trelloBoards.stream()
-               .filter(i ->i.getId()!=null)
-               .filter(i->i.getName()!=null)
-               .filter(i->i.getName().matches(".*Kodilla.*"))
-               .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+            trelloBoardDto.getLists().forEach(trelloList -> System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+
+            //trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+       // trelloBoards.stream()
+       //        .filter(i ->i.getId()!=null)
+       //        .filter(i->i.getName()!=null)
+       //       .filter(i->i.getName().matches(".*Kodilla.*"))
+       //        .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
+        });
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto){
+        return trelloClient.createNewCard(trelloCardDto);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getCardWithBadges")
+    public void getCardWithBadges()
+    {
+
     }
 }
--- src/main/java/com/crud/tasks/domain/CreatedBadges.java
+++ src/main/java/com/crud/tasks/domain/CreatedBadges.java
@@ -6,18 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class CreatedBadges {
 
-    @JsonProperty("name")
-    private String name;
-    @JsonProperty("id")
-    private String id;
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("votes")
+    private int votes;
+
+    @JsonProperty("AttachmentsByType")
+    private CreatedAttachments createdAttachments;
 }
